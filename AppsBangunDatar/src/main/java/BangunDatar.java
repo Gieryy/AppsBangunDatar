66db664abe9d9d149a4a07114271a8fd37429b4b
Gieryy <gierytian@gmail.com>
2022-12-20 10:46:34 +0700

    inisiasi project tugas akhir brow

---
+++ AppsBangunDatar/src/main/java/BangunDatar.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author DELL LATITUDE 7480
+ */
+public class BangunDatar {
+    
+}
